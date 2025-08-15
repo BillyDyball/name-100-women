87ce284dafeda9986ba2435d3f52e9b2bbf2def6
Billy Dyball <47304846+BillyDyball@users.noreply.github.com>
2025-08-16 00:41:55 +0100

    Add favicon and title to the Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,6 +13,10 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className={`${geistSans.className} ${geistMono.className}`}>
+      <head>
+        <title>Name 100 Women</title>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <NameGame />
     </div>
   );
