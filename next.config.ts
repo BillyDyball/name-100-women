8be0cc527af72b28f56c472cdc8ea46d2930b45b
Billy Dyball <47304846+BillyDyball@users.noreply.github.com>
2025-08-16 13:25:15 +0100

    Add redirect configuration for PostHog event tracking

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/relay-iT7i/:path*',
+        destination: `${process.env.NEXT_PUBLIC_POSTHOG_HOST}/:path*`,
+        permanent: true,
+      },
+    ];
+  }
 };
 
 export default nextConfig;
